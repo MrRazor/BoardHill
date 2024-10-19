1f3113661132a9140a3f8ea027102fa260daf015
MrRazor <jantomasstekl@gmail.com>
2024-10-19 21:55:32 +0200

    initial commit

---
+++ src/main/java/cz/uhk/boardhill/BoardHillApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.boardhill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardHillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardHillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/boardhill/BoardHillApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.boardhill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardHillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
