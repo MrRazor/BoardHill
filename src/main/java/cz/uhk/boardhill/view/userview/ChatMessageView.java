c1374bcc47d2ce5e2ca4de792f69a1046376ab02
MrRazor <jantomasstekl@gmail.com>
2024-12-13 23:24:55 +0100

    chat look improvement

--- src/main/java/cz/uhk/boardhill/view/userview/ChatMessageView.java
+++ src/main/java/cz/uhk/boardhill/view/userview/ChatMessageView.java
@@ -54,6 +54,9 @@ public class ChatMessageView extends VerticalLayout implements HasUrlParameter<S
     this.chatService = chatService;
     this.messageService = messageService;
     this.authContext = authContext;
+
+    setSizeFull();
+    setAlignItems(Alignment.CENTER);
   }
 
   @Override
@@ -78,58 +81,70 @@ public class ChatMessageView extends VerticalLayout implements HasUrlParameter<S
     username = authContext.getPrincipalName().get();
 
     Optional<Chat> chatOptional;
-    if(authorities.stream().map(a -> a.getAuthority()).anyMatch(a -> a.equals("ROLE_ADMIN"))) {
+    if (authorities.stream().map(a -> a.getAuthority()).anyMatch(a -> a.equals("ROLE_ADMIN"))) {
       chatOptional = chatService.findById(s);
-    }
-    else {
-      chatOptional = chatService.findAllNotDeletedChatsByUser(username).stream().filter(c->c.getName().equals(s)).findFirst();
+    } else {
+      chatOptional = chatService.findAllNotDeletedChatsByUser(username).stream().filter(c -> c.getName().equals(s)).findFirst();
     }
 
-    if(chatOptional.isPresent()) {
+    if (chatOptional.isPresent()) {
       chat = chatOptional.get();
 
       messageLayout = new VerticalLayout();
+      messageLayout.setWidth("50%");
+      messageLayout.setAlignItems(Alignment.STRETCH);
       add(messageLayout);
 
       reloadMessages();
 
       HorizontalLayout inputBar = new HorizontalLayout();
-      TextArea input = new TextArea("Enter Message");
-      Button sendMessage = new Button("Send Message");
-      sendMessage.addClickListener(e->{
-        if(input.getValue() != null && !input.getValue().equals("")) {
+      inputBar.setWidth("50%");
+      inputBar.setAlignItems(Alignment.END);
+
+      TextArea input = new TextArea();
+      input.setPlaceholder("Enter Message...");
+      input.setWidthFull();
+
+      Button sendMessage = new Button("Send");
+      sendMessage.addClickListener(e -> {
+        if (input.getValue() != null && !input.getValue().isEmpty()) {
           messageService.createMessage(s, username, input.getValue());
           MessageSentEventBroadcaster.broadcast(new MessageSentEvent(chat.getName()));
-          input.setValue("");
+          input.clear();
         }
       });
+
       inputBar.add(input, sendMessage);
+      inputBar.setFlexGrow(1, input);
       add(inputBar);
 
       HorizontalLayout pageBar = new HorizontalLayout();
-      Text pageText = new Text(Integer.toString(page+1));
+      pageBar.setAlignItems(Alignment.CENTER);
+
+      Text pageText = new Text(Integer.toString(page + 1));
       Button previousPage = new Button("Newer");
-      previousPage.addClickListener(e->{
-        if(page > 0) {
+      previousPage.addClickListener(e -> {
+        if (page > 0) {
           page--;
-          pageText.setText(Integer.toString(page+1));
+          pageText.setText(Integer.toString(page + 1));
           reloadMessages();
         }
       });
+
       Button nextPage = new Button("Older");
-      nextPage.addClickListener(e->{
-        if(messages.size() == PAGE_SIZE) {
+      nextPage.addClickListener(e -> {
+        if (messages.size() == PAGE_SIZE) {
           page++;
-          pageText.setText(Integer.toString(page+1));
+          pageText.setText(Integer.toString(page + 1));
           reloadMessages();
         }
       });
+
       pageBar.add(previousPage, pageText, nextPage);
       add(pageBar);
 
       fullyLoaded = true;
-    }
-    else {
+    } else {
       Notification notification = Notification.show("You cannot view this chat");
       notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
     }
@@ -141,25 +156,28 @@ public class ChatMessageView extends VerticalLayout implements HasUrlParameter<S
 
     messageLayout.removeAll();
 
-    for(Message message : messages) {
+    for (Message message : messages) {
       HorizontalLayout singleMessage = new HorizontalLayout();
+      singleMessage.setWidthFull();
+
       VerticalLayout messageContent = new VerticalLayout(
-        new Span("At: " + message.getCreatedAt().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))),
-        new Span("User: " + message.getUser().getUsername()),
-        new Span(message.getContent())
+              new Span("At: " + message.getCreatedAt().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))),
+              new Span("User: " + message.getUser().getUsername()),
+              new Span(message.getContent())
       );
       messageContent.setSpacing(false);
       messageContent.setPadding(false);
+      messageContent.setWidthFull();
 
       Button deleteMessageButton = new Button("Delete");
-      deleteMessageButton.addClickListener(e->{
+      deleteMessageButton.addClickListener(e -> {
         messageService.deleteMessage(message.getId(), username);
       });
-      if(!message.getUser().getUsername().equals(username) && !chat.getOwner().getUsername().equals(username)) {
+      if (!message.getUser().getUsername().equals(username) && !chat.getOwner().getUsername().equals(username)) {
         deleteMessageButton.setEnabled(false);
       }
-      singleMessage.add(deleteMessageButton, messageContent);
 
+      singleMessage.add(deleteMessageButton, messageContent);
       messageLayout.add(singleMessage);
     }
   }
