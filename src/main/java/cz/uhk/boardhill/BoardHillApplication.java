670062e9c52bcd98f5472907f85040400fe8caed
MrRazor <jantomasstekl@gmail.com>
2024-12-13 21:25:55 +0100

    showing message content, using vaadin push to update chat GUI

--- src/main/java/cz/uhk/boardhill/BoardHillApplication.java
+++ src/main/java/cz/uhk/boardhill/BoardHillApplication.java
@@ -1,10 +1,13 @@
 package cz.uhk.boardhill;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Push
 @SpringBootApplication
-public class BoardHillApplication {
+public class BoardHillApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardHillApplication.class, args);
--- src/main/java/cz/uhk/boardhill/view/userview/ChatMessageView.java
+++ src/main/java/cz/uhk/boardhill/view/userview/ChatMessageView.java
@@ -1,7 +1,10 @@
 package cz.uhk.boardhill.view.userview;
 
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -17,9 +20,11 @@ import cz.uhk.boardhill.service.ChatService;
 import cz.uhk.boardhill.service.MessageService;
 import cz.uhk.boardhill.view.MainLayout;
 import jakarta.annotation.security.RolesAllowed;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -32,7 +37,12 @@ public class ChatMessageView extends VerticalLayout implements HasUrlParameter<S
   private final MessageService messageService;
   private final AuthenticationContext authContext;
   private List<Message> messages;
-  int page=0;
+  private int page=0;
+  private final int PAGE_SIZE = 10;
+  private Chat chat;
+  private String username;
+  private VerticalLayout messageLayout;
+  private Timer timer;
 
   public ChatMessageView(ChatService chatService, MessageService messageService, AuthenticationContext authContext) {
     this.chatService = chatService;
@@ -41,25 +51,51 @@ public class ChatMessageView extends VerticalLayout implements HasUrlParameter<S
   }
 
   @Override
+  protected void onAttach(AttachEvent attachEvent) {
+    timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        attachEvent.getUI().access(()->reloadMessages());
+      }
+    }, 10000L, 10000L);
+  }
+
+  @Override
+  protected void onDetach(DetachEvent detachEvent) {
+    timer.cancel();
+    timer = null;
+  }
+
+  @Override
   public void setParameter(BeforeEvent beforeEvent, String s) {
     List<GrantedAuthority> authorities = authContext.getAuthenticatedUser(UserDetails.class).get().getAuthorities().stream().collect(Collectors.toList());
+    username = authContext.getPrincipalName().get();
 
     Optional<Chat> chatOptional;
     if(authorities.stream().map(a -> a.getAuthority()).anyMatch(a -> a.equals("ROLE_ADMIN"))) {
       chatOptional = chatService.findById(s);
     }
     else {
-      chatOptional = chatService.findAllNotDeletedChatsByUser(authContext.getPrincipalName().get()).stream().filter(c->c.getName().equals(s)).findFirst();
+      chatOptional = chatService.findAllNotDeletedChatsByUser(username).stream().filter(c->c.getName().equals(s)).findFirst();
     }
 
     if(chatOptional.isPresent()) {
+      chat = chatOptional.get();
+
+      messageLayout = new VerticalLayout();
+      add(messageLayout);
+
+      reloadMessages();
+
       HorizontalLayout inputBar = new HorizontalLayout();
       TextArea input = new TextArea("Enter Message");
       Button sendMessage = new Button("Send Message");
       sendMessage.addClickListener(e->{
         if(input.getValue() != null && !input.getValue().equals("")) {
-          messageService.createMessage(s, authContext.getPrincipalName().get(), input.getValue());
+          messageService.createMessage(s, username, input.getValue());
           input.setValue("");
+          reloadMessages();
         }
       });
       inputBar.add(input, sendMessage);
@@ -67,19 +103,21 @@ public class ChatMessageView extends VerticalLayout implements HasUrlParameter<S
 
       HorizontalLayout pageBar = new HorizontalLayout();
       Text pageText = new Text(Integer.toString(page+1));
-      Button previousPage = new Button("Previous Page");
+      Button previousPage = new Button("Newer");
       previousPage.addClickListener(e->{
         if(page > 0) {
           page--;
           pageText.setText(Integer.toString(page+1));
-          reloadMessages(s, page);
+          reloadMessages();
         }
       });
-      Button nextPage = new Button("Next Page");
+      Button nextPage = new Button("Older");
       nextPage.addClickListener(e->{
-        page++;
-        pageText.setText(Integer.toString(page+1));
-        reloadMessages(s, page);
+        if(messages.size() == PAGE_SIZE) {
+          page++;
+          pageText.setText(Integer.toString(page+1));
+          reloadMessages();
+        }
       });
       pageBar.add(previousPage, pageText, nextPage);
       add(pageBar);
@@ -90,8 +128,40 @@ public class ChatMessageView extends VerticalLayout implements HasUrlParameter<S
     }
   }
 
-  private void reloadMessages(String chatName, int page) {
-    messages = messageService.getMessages(chatName, page, 10).stream().collect(Collectors.toList());
+  private void reloadMessages() {
+    if(chat == null) {
+      return;
+    }
+    messages = messageService.getMessages(chat.getName(), page, PAGE_SIZE).stream().collect(Collectors.toCollection(new Supplier<List<Message>>() {
+      @Override
+      public List<Message> get() {
+        return new ArrayList<>();
+      }
+    }));
     Collections.reverse(messages);
+
+    messageLayout.removeAll();
+
+    for(Message message : messages) {
+      HorizontalLayout singleMessage = new HorizontalLayout();
+      VerticalLayout messageContent = new VerticalLayout(
+        new Span("At: " + message.getCreatedAt().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))),
+        new Span("User: " + message.getUser().getUsername()),
+        new Span(message.getContent())
+      );
+      messageContent.setSpacing(false);
+      messageContent.setPadding(false);
+
+      Button deleteMessageButton = new Button("Delete");
+      deleteMessageButton.addClickListener(e->{
+        messageService.deleteMessage(message.getId(), username);
+      });
+      if(!message.getUser().getUsername().equals(username) && !chat.getOwner().getUsername().equals(username)) {
+        deleteMessageButton.setEnabled(false);
+      }
+      singleMessage.add(deleteMessageButton, messageContent);
+
+      messageLayout.add(singleMessage);
+    }
   }
 }
