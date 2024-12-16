50daa63d978e8ecae74d505426d01e6681002f76
MrRazor <jantomasstekl@gmail.com>
2024-12-16 23:44:03 +0100

    fix vaadin prepare frontend, be able to build war file for tomcat

--- src/main/java/cz/uhk/boardhill/BoardHillApplication.java
+++ src/main/java/cz/uhk/boardhill/BoardHillApplication.java
@@ -2,49 +2,22 @@ package cz.uhk.boardhill;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import cz.uhk.boardhill.service.UserService;
-import lombok.AllArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.time.ZoneId;
 import java.util.Locale;
-import java.util.TimeZone;
 
 @Push
-@AllArgsConstructor
 @SpringBootApplication
-public class BoardHillApplication implements CommandLineRunner, AppShellConfigurator {
+public class BoardHillApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static final ZoneId VIEW_TZ = ZoneId.of("CET");
     public static final Locale VIEW_LOCALE = Locale.UK;
-
-    private static final Logger LOGGER = LogManager.getLogger(BoardHillApplication.class);
-    private final UserService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(BoardHillApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("UTC")));
-        Locale.setDefault(Locale.UK);
-
-        try {
-            userService.register("admin", "password", true);
-            LOGGER.info("Default user with admin rights (username: \"admin\", password: \"password\") created.");
-        } catch (Exception e) {
-            LOGGER.info("Default user with admin rights (username: \"admin\", password: \"password\") was not created, probably already exists (is created at first run of application).");
-        }
-        try {
-            userService.register("user", "password", false);
-            LOGGER.info("Default user without admin rights (username: \"user\", password: \"password\") created.");
-        } catch (Exception e) {
-            LOGGER.info("Default user without admin rights (username: \"user\", password: \"password\") was not created, probably already exists (is created at first run of application).");
-        }
-    }
 }
---
+++ src/main/java/cz/uhk/boardhill/CommandLineRunnerImpl.java
@@ -0,0 +1,39 @@
+package cz.uhk.boardhill;
+
+import cz.uhk.boardhill.service.UserService;
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.util.Locale;
+import java.util.TimeZone;
+
+@AllArgsConstructor
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private static final Logger LOGGER = LogManager.getLogger(CommandLineRunnerImpl.class);
+    private final UserService userService;
+
+    @Override
+    public void run(String... args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("UTC")));
+        Locale.setDefault(Locale.UK);
+
+        try {
+            userService.register("admin", "password", true);
+            LOGGER.info("Default user with admin rights (username: \"admin\", password: \"password\") created.");
+        } catch (Exception e) {
+            LOGGER.info("Default user with admin rights (username: \"admin\", password: \"password\") was not created, probably already exists (is created at first run of application).");
+        }
+        try {
+            userService.register("user", "password", false);
+            LOGGER.info("Default user without admin rights (username: \"user\", password: \"password\") created.");
+        } catch (Exception e) {
+            LOGGER.info("Default user without admin rights (username: \"user\", password: \"password\") was not created, probably already exists (is created at first run of application).");
+        }
+    }
+}
